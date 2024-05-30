d89dd630f20f07b8b25a203381abaf788b015e9c
Charlie Willoughby <104572309+CWilloughby98@users.noreply.github.com>
2024-05-30 16:26:12 +0000

    database + endpoints client & artist

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User_Client, User_Artist
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -12,11 +12,60 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/user_client', methods=['POST', 'GET'])
+def create_client():
+    data = request.json
+    email = data.get("email")
+    password = data.get("password")
+    first_name = data.get("first_name")
+    last_name = data.get("last_name")
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    if not email or not password:
+        return jsonify({"Error": "Username or Password are required"}), 400
+    if User_Client.query.filter_by(email = email).first():
+        return jsonify({"Error": "User already exists"}), 400
+    
+    ## Hay que encriptar la contarseña en el front, así al enviar los datos al endpoint
+    ## viene cifrada en el payload.
 
-    return jsonify(response_body), 200
+    new_user = User_Client(email = email, password = password, first_name = first_name, last_name = last_name)
+
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+    except Exception as ex:
+        db.session.rollback()
+        return jsonify({"Message": "Something went wrong", "Error": str(ex)}), 500
+    
+    return jsonify(new_user.serialize()), 201
+
+@api.route('/user_artist', methods=['POST', 'GET'])
+def create_artist():
+    data = request.json
+    email = data.get("email")
+    password = data.get("password")
+    first_name = data.get("first_name")
+    last_name = data.get("last_name")
+    description = data.get("description")
+
+    if not email or not password:
+        return jsonify({"Error": "Username or Password are required"}), 400
+    if User_Artist.query.filter_by(email = email).first():
+        return jsonify({"Error": "User already exists"}), 400
+    
+    ## Hay que encriptar la contarseña en el front, así al enviar los datos al endpoint
+    ## viene cifrada en el payload.
+
+    new_user = User_Artist(email = email, password = password, first_name = first_name, 
+                           last_name = last_name, description = description)
+
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+    except Exception as ex:
+        db.session.rollback()
+        return jsonify({"Message": "Something went wrong", "Error": str(ex)}), 500
+    
+    return jsonify(new_user.serialize()), 201
+
+## Crear tokens al hacer el incio de sesión
