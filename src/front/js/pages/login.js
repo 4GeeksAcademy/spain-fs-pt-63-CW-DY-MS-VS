62ca4dbebe5ff2917dd96340cafa363cb7cb2ca7
Dayloc <158208881+Dayloc@users.noreply.github.com>
2024-06-14 23:23:26 +0000

    hay que cambiar un detalle

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,19 +1,21 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/login.css";
-import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import { Link, Navigate } from "react-router-dom";
 
 const Login = () => {
   const { store, actions } = useContext(Context);
   const [user, setUser] = useState({ email: "", password: "", userType: "" })
-
+  const navigate = useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault();
 
     actions.login(user);
-    setUser({ email: "", password: "", userType: "" })
-
-  };
+    setUser({ email: "", password: "", userType: "" });
+    
+     };
+     
 console.log(store.token)
   return (
     <div className="container text-center">
@@ -70,10 +72,10 @@ console.log(store.token)
           
       
           <button className="mt-5  go" type="submit">
-            Entrar Datos
+            Enviar Datos
           </button>
-          <div>     <Link to={"/"}> <button className="mt-5 mb-5 go">Gooooo!!!!</button></Link></div>
-          <Link to="/register"><p>¡Register here!</p></Link>
+          <div>     <Link to={"/"}> <button className="mt-5 mb-5 go">Aceptar</button></Link></div>
+          <Link to="/register"><p>¡Register here!</p></Link>  
         </div>
      
       </form>
