354bff77be921208a340e9020baa733e22820040
mariasr79 <147938132+mariasr79@users.noreply.github.com>
2024-06-20 19:47:40 +0000

    vista obra mejorada

--- src/front/js/pages/workDetail.js
+++ src/front/js/pages/workDetail.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaRegHeart } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+import { IoCartSharp } from "react-icons/io5";
 import "../../styles/workDetail.css";
 import obraImagen from "../../img/creacion adan.jpeg";
 
 const WorkDetail = ({ obra }) => {
+  const [likeCount, setLikeCount] = useState(200);
+  const [liked, setLiked] = useState(false);
+
+  const incrementLike = () => {
+    if (!liked) {
+      setLikeCount(likeCount + 1);
+    } else {
+      setLikeCount(likeCount - 1);
+    }
+    setLiked(!liked);
+  };
+
   return (
     <div className="container mt-4">
       <div className="work-detail">
@@ -21,14 +36,23 @@ const WorkDetail = ({ obra }) => {
             <strong>Precio: </strong>30.000 €
           </p>
           <p className="like-count">
-            <strong>❤️ 200</strong>
-            
+            <strong>
+              {!liked ? <FaRegHeart /> : <FaHeart />}
+              {" "}
+              {likeCount}
+            </strong>
           </p>
-          
+          <div className="buttons">
+            <button className="like-button" onClick={incrementLike}>
+              <FaHeart />
+            </button>
+            <button className="cart-button">
+              <IoCartSharp /> Añadir al carrito
+            </button>
+          </div>
         </div>
       </div>
     </div>
   );
 };
-
 export default WorkDetail;
