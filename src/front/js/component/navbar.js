e7c9a1ddb365763f23097bb673ae8edd1c8765aa
mariasr79 <147938132+mariasr79@users.noreply.github.com>
2024-06-22 10:57:42 +0000

    cambios css navbar 22/6(2)

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -5,9 +5,9 @@ import { IoFlowerOutline } from "react-icons/io5";
 export const Navbar = () => {
   return (
     <nav className="navbar navbar-dark navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
+      <div className="container-fluid p-0">
         <IoFlowerOutline />
-        <button className="navbar-toggler mt-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
