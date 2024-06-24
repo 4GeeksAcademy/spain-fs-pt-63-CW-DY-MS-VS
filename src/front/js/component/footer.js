1bcba5952e78040161a52bcf97b3a610c27c1c96
mariasr79 <147938132+mariasr79@users.noreply.github.com>
2024-06-24 15:39:14 +0000

    footer mejorado 24/6(0)

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "../../styles/home.css"
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
+	<footer className="footer mt-auto py-0 text-center">
 		<p>
 			Made with  <i className="fa fa-heart text-danger" />  by{" Charlie, Varos, Dayloc y María "}
 			
