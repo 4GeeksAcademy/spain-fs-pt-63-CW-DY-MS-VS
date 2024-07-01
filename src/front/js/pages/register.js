c6b7dfe2af49df8fa637fd598812f8193e692ac0
Varos1009 <85431234+Varos1009@users.noreply.github.com>
2024-07-01 16:59:55 +0000

    mas cambios

--- src/front/js/pages/register.js
+++ src/front/js/pages/register.js
@@ -63,8 +63,8 @@ export const Register = () => {
         <div className="my-5 mt-5">
 
             <form onSubmit={handleSubmit} >
-                <div className="row mb-3 mt-5 text-center mb-3">
-                    <h3>Register</h3>
+                <div className="row mb-3 mt-5 text-center " >
+                    <h3 className="mt-5">Register</h3>
                     <div className="col-sm-5 m-auto">
                         <input type="name" className="form-control " id="inputName3"
                             value={first_name}
